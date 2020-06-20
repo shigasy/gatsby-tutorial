ab2ab2c88a13be695297b66f55bfd246b3b76baf
shigasy <kxx555u.u@gmail.com>
2020-06-20 12:40:28 +0900

    add: 4章 emotion使ったり、typograpyのテーマ変えたり、graphqlで静的なデータを読んだり、ページコンポーネント以外でgraphqlのquery使ったり

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,9 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
   plugins: [
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
